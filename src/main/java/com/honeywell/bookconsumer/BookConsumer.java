7534e8f34463f563a1b6a7656b78f05bb3a30949
Witek, Jarosław Piotr <jaroslaw.witek@ge.com>
2020-08-28 09:52:28 +0200

    Added self-baked JWT

--- src/main/java/com/honeywell/bookconsumer/BookConsumer.java
+++ src/main/java/com/honeywell/bookconsumer/BookConsumer.java
@@ -1,5 +1,7 @@
 package com.honeywell.bookconsumer;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 
 
 @Configuration
@@ -20,12 +23,19 @@ public class BookConsumer {
     @Value("${token.stored.value}")
     String tokenValue;
 
+    private static final String JWT_KEY = "super secret key";
+
     @EventListener(ApplicationStartedEvent.class)
     public void doStuff(){
-        String authenticationToken ="Bearer "+tokenValue;
+//        String authenticationToken ="Bearer "+bakeJWT();
         consumeAll();
-        addBook("Przygody dobrego wojaka Szwejka", authenticationToken);
-        addBook("XYZ", authenticationToken);
+        addBook("Przygody dobrego wojaka Szwejka");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        addBook("XYZ");
         consumeAll();
     }
 
@@ -35,9 +45,9 @@ public class BookConsumer {
         System.out.println("Fetched books:"+System.lineSeparator()+booksResponse);
     }
 
-    public void addBook(String title, String authenticationToken){
+    public void addBook(String title){
         HttpHeaders headers = new HttpHeaders();
-
+        String authenticationToken ="Bearer "+bakeJWT();
         headers.add("Authorization", authenticationToken);
         headers.add("Content-Type", "application/json");
 
@@ -45,5 +55,12 @@ public class BookConsumer {
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.exchange(targetEndpoint, HttpMethod.POST,entity, String.class);
+    }
+
+    private String bakeJWT(){
+        Algorithm algo = Algorithm.HMAC256(JWT_KEY);
+        String token = JWT.create().withSubject("application name").withIssuedAt(new Date()).withExpiresAt(new Date(System.currentTimeMillis()+10000)).sign(algo);
+        System.out.println("Token{"+token+"}");
+        return token;
     }
 }
